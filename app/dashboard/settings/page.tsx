08a006b0bfebb1b7ce2cc7a4b29f63dd1ff31f15
李飞 <634874196@qq.com>
2024-01-19 11:19:12 +0800

    feat: metadata

--- app/dashboard/settings/page.tsx
+++ app/dashboard/settings/page.tsx
@@ -1,3 +1,9 @@
+
+import type { Metadata } from 'next'
+export const metadata: Metadata = {
+  title: 'settings',
+  description: 'settings pages',
+}
 export default () => {
   return <h1>Hello, settings Page!</h1>
 }
